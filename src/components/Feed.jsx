8557832e3be5c9bd08d713db69cca50a0d39f95d
Aman Rawat <amanrawat4060@gmail.com>
2022-04-25 14:56:59 +0530

    added a sanity query in data.js in utils folder and added a loading spinner

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 function Feed() {
-  return (
-    <div>Feed</div>
-  )
+
+    // Loading Spinner State
+    const [loading, setLoading] = useState(false)
+    const { categoryId } = useParams()
+
+    useEffect(() => {
+        setLoading(true)
+        if (categoryId) {
+            const query = searchQuery(categoryId)
+        } else {
+
+        }
+    }, [categoryId])
+    
+
+    if(loading) return <Spinner message="We are adding new ideas to your feed"/>
+    return <div>Feed</div>;
 }
 
-export default Feed
+export default Feed;
