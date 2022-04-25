4035cdc65ba7e37b6ee6f64c57ca302508e9e5ab
Aman Rawat <amanrawat4060@gmail.com>
2022-04-25 11:51:57 +0530

    added 5 new components and created their routes in Pins.jsx

--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-function Pins() {
-  return (
-    <div>Pins</div>
-  )
+import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components";
+
+function Pins({ user }) {
+
+    const [searchTerm, setSearchTerm] = useState("")
+
+    return (
+        <div className="px-2 md:px-5">
+            <div className="bg-gray-50">
+                <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
+            <div className="h-full ">
+                <Routes>
+                    <Route path="/" element={<Feed />} />
+                    <Route path="/category/:categoryId" element={<Feed />} />
+                    <Route path="/Pin-detail/:pinId" element={<PinDetail user={user} />} />
+                    <Route path="/create-pin" element={<CreatePin user={user} />} />
+                    <Route path="/search" element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
-export default Pins
+export default Pins;
